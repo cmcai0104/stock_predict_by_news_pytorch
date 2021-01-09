9b97bdff9a296edd672c2c29d8782f6aa1eddd87
C.M.Cai <hzxsccm678335@163.com>
2021-01-09 10:54:54 +0800

    create repo

--- base_functions.py
+++ base_functions.py
@@ -5,7 +5,7 @@ import tushare as ts
 from datetime import datetime
 import re
 import torch
-from torch.utils.data import TensorDataset, DataLoader, RandomSampler
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 from transformers import BertTokenizer
 from tqdm import notebook, trange
 import matplotlib.pyplot as plt
@@ -16,6 +16,7 @@ def affected_date(series: pd.Series, trade_dates: pd.Series, deadline='15:00:00'
     """
     根据新闻的创建时间获得该新闻影响的交易日
     Args:
+        deadline: 新闻归入当前交易日的截止时间
         series: 一条新闻
         trade_dates: 交易日序列
 
@@ -34,6 +35,7 @@ def data_preprocess(path="./data/history_news.txt", deadline='15:00:00'):
     """
     读取数据，关联上证综指，并进行数据预处理。
     Args:
+        deadline: 归入下一个交易日的截止时间
         path: 历史新闻保存路径
 
     Returns: DataFrame
@@ -58,111 +60,95 @@ def data_preprocess(path="./data/history_news.txt", deadline='15:00:00'):
     return df
 
 
-def daily_filter(daily_df: pd.DataFrame, tokenizer: BertTokenizer, max_sent_num: int, max_sent_length: int):
+def daily_filter(daily_df: pd.DataFrame, tokenizer: BertTokenizer, max_news_num: int, max_news_length: int):
     """
     对每个交易日的新闻进行indices，并保存每条新闻词的数量，以及当天上证综指涨跌幅/10
     Args:
+        tokenizer:
         daily_df:
-        max_sent_num:
-        max_sent_length:
+        max_news_num:
+        max_news_length:
 
     Returns:
 
     """
     daily_df = daily_df.sort_values(by=['share_num', 'recommend_num', 'reading_num'], ascending=False)
     daily_df = daily_df[['create_time', 'title', 'reading_num', 'share_num', 'recommend_num', 'affected_date']]
-    daily_df = daily_df.iloc[:max_sent_num, :]
+    news_num = len(daily_df)
+    daily_df = daily_df.iloc[:max_news_num, :]
     sentences = daily_df['title'].to_list()
     sentences = [re.sub(r"(\u3000|\n|\t|\r|：|)", "", sent) for sent in sentences]
-    text = []
-    atten_masks = []
-    sent_len = []
+    news_length = max([len(sent) for sent in sentences])
+    input_ids = []
+    att_masks = []
     for sent in sentences:
-        indices = tokenizer.encode(sent[:max_sent_length])
-        seq_mask = [1.] * len(indices)
-        seq_mask.extend([0.] * (max_sent_length + 2 - len(indices)))
-        atten_masks.append(seq_mask)
-        sent_len.append(len(indices))
-        if len(indices) < max_sent_length + 2:
-            indices.extend([0] * (max_sent_length + 2 - len(indices)))
-        text.append(indices)
-    while len(text) < max_sent_num:
-        text.append([0] * (max_sent_length + 2))
-        atten_masks.append([0.] * (max_sent_length + 2))
-        sent_len.append(0)
-    return text, atten_masks, sent_len
+        encode_sent = tokenizer.encode(sent, add_special_tokens=True, padding='max_length',
+                                       truncation='longest_first', max_length=max_news_length)
+        input_ids.append(encode_sent)
+        att_mask = [int(token_id > 0) for token_id in encode_sent]
+        att_masks.append(att_mask)
+    while len(input_ids) < max_news_num:
+        input_ids.append([0] * max_news_length)
+        att_masks.append([0] * max_news_length)
+    return input_ids, att_masks, news_num, news_length
 
 
-def data_split(df, tokenizer, max_sent_num, max_sent_length, batch_size):
+def generate_dataloader(input_ids, att_masks, labels, batch_size):
+    input_ids = torch.tensor(input_ids)
+    att_masks = torch.tensor(att_masks)
+    labels = torch.tensor(labels).float()
+    data = TensorDataset(input_ids, att_masks, labels)
+    sampler = RandomSampler(data)
+    dataloader = DataLoader(data, sampler=sampler, batch_size=batch_size)
+    return dataloader
+
+
+def data_split(df, tokenizer, max_news_num, max_news_length, batch_size):
     """
     Spliting the DataFrame into training, validation and test. Encoding the texts by tokenizer,
     and create the masks and sentences length list at the same time.
     Args:
         df: Dataframe
         tokenizer: encoder tokenizer
-        max_sent_num: max sentence number
-        max_sent_length: max sentence length
+        max_news_num: max sentence number
+        max_news_length: max sentence length
         batch_size:
 
     Returns: Dataloader, sentences_length(inclouding training, validation and test)
     """
     print('Encoding texts by tokenizer, and splitting the dataset into training, valid and test......')
-    texts_train = []
-    texts_valid = []
-    texts_test = []
-    atten_masks_train = []
-    atten_masks_valid = []
-    atten_masks_test = []
-    sentences_length_train = []
-    sentences_length_valid = []
-    sentences_length_test = []
-    labels_train = []
-    labels_valid = []
-    labels_test = []
+    train_input_ids = []
+    valid_input_ids = []
+    test_input_ids = []
+    train_att_masks = []
+    valid_att_masks = []
+    test_att_masks = []
+    train_labels = []
+    valid_labels = []
+    test_labels = []
     daily_df_list = [daily_df[1] for daily_df in df.groupby('affected_date')]
     for daily_df in daily_df_list:
-        text, atten_mask, sent_len = daily_filter(daily_df, tokenizer=tokenizer, max_sent_num=max_sent_num,
-                                                  max_sent_length=max_sent_length)
+        input_id, att_mask, news_num, news_length = daily_filter(daily_df, tokenizer=tokenizer,
+                                                                 max_news_num=max_news_num,
+                                                                 max_news_length=max_news_length)
         if daily_df['affected_date'].unique()[0] < np.datetime64('2020-01-01'):
-            texts_train.append(text)
-            atten_masks_train.append(atten_mask)
-            sentences_length_train.append(sent_len)
-            labels_train.append(daily_df['pct_chg'].unique() / 10)
+            train_input_ids.append(input_id)
+            train_att_masks.append(att_mask)
+            train_labels.append(daily_df['pct_chg'].unique() / 10)
         elif daily_df['affected_date'].unique()[0] < np.datetime64('2020-07-01'):
-            texts_valid.append(text)
-            atten_masks_valid.append(atten_mask)
-            sentences_length_valid.append(sent_len)
-            labels_valid.append(daily_df['pct_chg'].unique() / 10)
+            train_input_ids.append(input_id)
+            train_att_masks.append(att_mask)
+            train_labels.append(daily_df['pct_chg'].unique() / 10)
         else:
-            texts_test.append(text)
-            atten_masks_test.append(atten_mask)
-            sentences_length_test.append(sent_len)
-            labels_test.append(daily_df['pct_chg'].unique() / 10)
-    texts_train = torch.tensor(texts_train)
-    texts_valid = torch.tensor(texts_valid)
-    texts_test = torch.tensor(texts_test)
-    atten_masks_train = torch.tensor(atten_masks_train)
-    atten_masks_valid = torch.tensor(atten_masks_valid)
-    atten_masks_test = torch.tensor(atten_masks_test)
-    sentences_length_train = torch.tensor(sentences_length_train)
-    sentences_length_valid = torch.tensor(sentences_length_valid)
-    sentences_length_test = torch.tensor(sentences_length_test)
-    labels_train = torch.tensor(labels_train).float()
-    labels_valid = torch.tensor(labels_valid).float()
-    labels_test = torch.tensor(labels_test).float()
+            train_input_ids.append(input_id)
+            train_att_masks.append(att_mask)
+            train_labels.append(daily_df['pct_chg'].unique() / 10)
 
-    train_data = TensorDataset(texts_train, atten_masks_train, labels_train)
-    train_sampler = RandomSampler(train_data)
-    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+    train_dataloader = generate_dataloader(train_input_ids, train_att_masks, train_labels, batch_size)
+    valid_dataloader = generate_dataloader(valid_input_ids, valid_att_masks, valid_labels, batch_size)
+    test_dataloader = generate_dataloader(test_input_ids, test_att_masks, test_labels, batch_size)
 
-    valid_data = TensorDataset(texts_valid, atten_masks_valid, labels_valid)
-    valid_sampler = RandomSampler(valid_data)
-    valid_dataloader = DataLoader(valid_data, sampler=valid_sampler, batch_size=batch_size)
-
-    test_data = TensorDataset(texts_test, atten_masks_test, labels_test)
-    test_sampler = RandomSampler(test_data)
-    test_dataloader = DataLoader(test_data, sampler=test_sampler, batch_size=batch_size)
-    return train_dataloader, sentences_length_train, valid_dataloader, sentences_length_valid, test_dataloader, sentences_length_test
+    return train_dataloader, valid_dataloader, test_dataloader
 
 
 def save_checkpoint(save_path, model, valid_loss):
@@ -227,9 +213,9 @@ def train(model, optimizer, train_loader, valid_loader, train_epochs, save_file_
 
     model.train()
     for epoch in trange(int(train_epochs), desc='Epoch'):
-        for _, batch in enumerate(notebook.tqdm(train_loader)):
-            batch = tuple(t.to(device) for t in batch)
-            input_ids, input_mask, label_ids = batch
+        for _, train_batch in enumerate(notebook.tqdm(train_loader)):
+            train_batch = tuple(t.to(device) for t in train_batch)
+            input_ids, input_mask, label_ids = train_batch
             y_predict = model(input_ids, input_mask)
             loss = torch.nn.functional.mse_loss(y_predict, label_ids)
 
@@ -244,9 +230,9 @@ def train(model, optimizer, train_loader, valid_loader, train_epochs, save_file_
             if global_step % eval_every == 0:
                 model.eval()
                 with torch.no_grad():
-                    for _, batch in enumerate(valid_loader):
-                        batch = tuple(t.to(device) for t in batch)
-                        input_ids, input_mask, label_ids = batch
+                    for _, valid_batch in enumerate(valid_loader):
+                        valid_batch = tuple(t.to(device) for t in valid_batch)
+                        input_ids, input_mask, label_ids = valid_batch
                         y_predict = model(input_ids, input_mask)
                         loss = torch.nn.functional.mse_loss(y_predict, label_ids)
                         valid_running_loss += loss.item()
@@ -283,21 +269,20 @@ def evaluate(model, test_loader, device):
 
     model.eval()
     with torch.no_grad():
-        for (labels, title, text, titletext), _ in test_loader:
-            labels = labels.type(torch.LongTensor).to(device)
-            titletext = titletext.type(torch.LongTensor).to(device)
-            loss, _ = model(titletext, labels)
-            y_pred.extend(torch.argmax(loss, 1).tolist())
-            y_true.extend(labels.tolist())
+        for _, batch in enumerate(test_loader):
+            batch = tuple(t.to(device) for t in batch)
+            input_ids, input_mask, label_ids = batch
+            y_predict = model(input_ids, input_mask)
+            y_pred.extend(y_predict.tolist())
+            y_true.extend(label_ids.tolist())
 
-    print('Classification Report:')
-    print(classification_report(y_true, y_pred, label=[1, 0], digits=4))
-
-    cm = confusion_matrix(y_true, y_pred, labels=[1, 0])
-    ax = plt.subplot()
-    sns.heatmap(cm, annot=True, ax=ax, cmap='Blues', fmt='d')
-    ax.set_title()
-    ax.set_xlabel()
-    ax.set_ylabel()
-    ax.xaxis.set_ticklabels(['FAKE', 'REAL'])
-    ax.yaxis.set_ticklabels(['FAKE', 'REAL'])
+    # print('Classification Report:')
+    # print(classification_report(y_true, y_pred, label=[1, 0], digits=4))
+    # cm = confusion_matrix(y_true, y_pred, labels=[1, 0])
+    # ax = plt.subplot()
+    # sns.heatmap(cm, annot=True, ax=ax, cmap='Blues', fmt='d')
+    # ax.set_title()
+    # ax.set_xlabel()
+    # ax.set_ylabel()
+    # ax.xaxis.set_ticklabels(['FAKE', 'REAL'])
+    # ax.yaxis.set_ticklabels(['FAKE', 'REAL'])
--- training.py
+++ training.py
@@ -13,11 +13,25 @@ from transformers import get_linear_schedule_with_warmup
 from neural_network import FinBertTransformer
 from base_functions import data_preprocess, data_split, train
 
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+    print('There are %d GPU(s) available.' % torch.cuda.device_count())
+    print('We will use the GPu:', torch.cuda.get_device_name(0))
+else:
+    device = torch.device('cpu')
+    print('No GPU available, using the CPU instead.')
+
 if __name__ == '__main__':
+    SEED = 333
+    random.seed(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+
     MAX_SENT_LENGTH = 50
     MAX_SENT_NUM = 50
     BATCH_SIZE = 2
-    EPOCHS = 100
+    EPOCHS = 10
     LEARNING_RATE = 2e-5
     WEIGHT_DECAY = 1e-2
     EPSILION = 1e-8
@@ -27,23 +41,25 @@ if __name__ == '__main__':
     global_step = 0
     df = data_preprocess(path="./data/history_news.txt", deadline='15:00:00')
     model_path = './pretrained_models/FinBERT_L-12_H-768_A-12_pytorch'
+    print('Loading BERT tokenizer from <==', model_path)
     tokenizer = BertTokenizer.from_pretrained(model_path)
-    train_dataloader, _, valid_dataloader, _, test_dataloader, _ = data_split(df, tokenizer=tokenizer,
-                                                                              max_sent_num=MAX_SENT_NUM,
-                                                                              max_sent_length=MAX_SENT_LENGTH,
-                                                                              batch_size=BATCH_SIZE)
+    train_dataloader, valid_dataloader, test_dataloader = data_split(df, tokenizer=tokenizer,
+                                                                     max_news_num=MAX_SENT_NUM,
+                                                                     max_news_length=MAX_SENT_LENGTH,
+                                                                     batch_size=BATCH_SIZE)
     del df
     print('Building the model and load pre-trained parameters......')
     model = FinBertTransformer(pretrain_path=model_path, sents_num=MAX_SENT_NUM, sent_hidden=[96, 24],
                                nhead=1, num_layers=1, news_hidden=[12, 1])
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
     model.to(device)
     optimizer = AdamW(model.parameters(), lr=LEARNING_RATE, eps=EPSILION)
+    total_steps = len(train_dataloader) * EPOCHS
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=total_steps)
+
     train(model=model, optimizer=optimizer, train_loader=train_dataloader, valid_loader=valid_dataloader,
           train_epochs=EPOCHS, save_file_path='./model', device=device)
 
-    total_steps = len(train_dataloader) * EPOCHS
-    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=total_steps)
     model.train()
 
     for i in trange(int(EPOCHS), desc='Epoch'):
