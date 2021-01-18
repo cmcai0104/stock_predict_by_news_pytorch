097af7503f9b0b908c47bc8c078733003c98382a
cmcai0104 <hzxsccm678335@163.com>
2021-01-18 16:18:33 +0800

    add training reslut

--- base_functions.py
+++ base_functions.py
@@ -58,7 +58,7 @@ def data_preprocess(path="./data/history_news.txt", deadline='15:00:00'):
     szzz = szzz[['trade_date', 'pct_chg']].astype({'trade_date': 'datetime64', 'pct_chg': 'float'})
     df['affected_date'] = df.apply(lambda x: affected_date(x, trade_dates=szzz['trade_date'], deadline=deadline),
                                    axis=1)
-    df = df.merge(szzz, how='left', left_on='affected_date', right_on='trade_date')
+    df = df.merge(szzz, how='left', left_on='affected_date', right_on='trade_date').sort_values(by=['affected_date'], ascending=True)
     return df
 
 
@@ -95,13 +95,16 @@ def daily_filter(daily_df: pd.DataFrame, tokenizer: BertTokenizer, max_news_num:
     return input_ids, att_masks, news_num, news_length
 
 
-def generate_dataloader(input_ids, att_masks, labels, batch_size):
+def generate_dataloader(input_ids, att_masks, labels, batch_size, shuffle=True):
     input_ids = torch.tensor(input_ids)
     att_masks = torch.tensor(att_masks)
     labels = torch.tensor(labels).float()
     data = TensorDataset(input_ids, att_masks, labels)
-    sampler = RandomSampler(data)
-    dataloader = DataLoader(data, sampler=sampler, batch_size=batch_size)
+    if shuffle:
+        sampler = RandomSampler(data)
+        dataloader = DataLoader(data, sampler=sampler, batch_size=batch_size)
+    else:
+        dataloader = DataLoader(data, batch_size=batch_size, shuffle=shuffle)
     return dataloader
 
 
@@ -130,6 +133,7 @@ def data_split(df, tokenizer, max_news_num, max_news_length, batch_size):
     train_labels = []
     valid_labels = []
     test_labels = []
+
     daily_df_list = [daily_df[1] for daily_df in df.groupby('affected_date')]
     for daily_df in daily_df_list:
         input_id, att_mask, news_num, news_length = daily_filter(daily_df, tokenizer=tokenizer,
@@ -145,7 +149,7 @@ def data_split(df, tokenizer, max_news_num, max_news_length, batch_size):
             valid_input_ids.append(input_id)
             valid_att_masks.append(att_mask)
             valid_labels.append(daily_df['pct_chg'].unique() / 10)
-        else:
+        elif daily_df['affected_date'].unique()[0] < np.datetime64('2021-01-01'):
             test_input_ids.append(input_id)
             test_att_masks.append(att_mask)
             test_labels.append(daily_df['pct_chg'].unique() / 10)
@@ -155,7 +159,7 @@ def data_split(df, tokenizer, max_news_num, max_news_length, batch_size):
     print("The day with the most news releases was %s, we just take the top %s." % (max(news_num_list), max_news_num))
     train_dataloader = generate_dataloader(train_input_ids, train_att_masks, train_labels, batch_size)
     valid_dataloader = generate_dataloader(valid_input_ids, valid_att_masks, valid_labels, batch_size)
-    test_dataloader = generate_dataloader(test_input_ids, test_att_masks, test_labels, batch_size)
+    test_dataloader = generate_dataloader(test_input_ids, test_att_masks, test_labels, batch_size, shuffle=False)
 
     return train_dataloader, valid_dataloader, test_dataloader
 
@@ -201,20 +205,19 @@ def load_metrics(load_path, device):
     return state_dict['train_loss_list'], state_dict['valid_loss_list'], state_dict['global_steps_list']
 
 
-def training_history_plot(plt_save_path, destination_folder, device):
-    train_loss_list, valid_loss_list, global_steps_list = load_metrics(destination_folder + '/metrics.pt', device)
+def training_history_plot(plt_save_path,train_loss_list, valid_loss_list, global_steps_list):
     plt.plot(global_steps_list, train_loss_list, label='Train')
     plt.plot(global_steps_list, valid_loss_list, label='Valid')
     plt.title('Training and Validation loss of the model')
     plt.xlabel('Global Steps')
     plt.ylabel('Loss')
     plt.legend()
-    plt.savefig(plt_save_path)
+    plt.savefig(plt_save_path + '/evaluation/loss.png')
     plt.show()
     print('The training and valid loss curves have already saved to ==> {plt_save_path}')
 
 
-def train(model, optimizer, scheduler, epochs, train_dataloader, valid_dataloader, device, save_file_path='./training_information'):
+def train(model, optimizer, scheduler, epochs, train_dataloader, valid_dataloader, device, save_file_path='.'):
     print('Start training the model!')
     best_valid_loss = float("Inf")
     train_loss_list = []
@@ -229,9 +232,10 @@ def train(model, optimizer, scheduler, epochs, train_dataloader, valid_dataloade
         for step, train_batch in enumerate(train_dataloader):
             if step % 10 == 0 and not step == 0:
                 elapsed = format_time(time.time() - t0)
-                print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(train_dataloader), elapsed))
+                print("\r", 'Batch {:>5,} of {:>5,}. Elapsed: {:}.'.format(step, len(train_dataloader), elapsed), end="",flush=True)
             train_batch = tuple(inp.to(device) for inp in train_batch)
             input_ids, input_mask, label_ids = train_batch
+            del train_batch
             y_predict = model(input_ids, input_mask)
             loss = torch.nn.functional.mse_loss(y_predict, label_ids)
             total_loss += loss.item()
@@ -244,8 +248,8 @@ def train(model, optimizer, scheduler, epochs, train_dataloader, valid_dataloade
         avg_train_loss = total_loss / len(train_dataloader)
         train_loss_list.append(avg_train_loss)
         print("")
-        print("  Average training loss: {0:.2f}".format(avg_train_loss))
-        print("  Training epoch took: {:}".format(format_time(time.time() - t0)))
+        print(" Average training loss: {0:.5f}".format(avg_train_loss))
+        print(" Training epoch took: {:}".format(format_time(time.time() - t0)))
 
         print("")
         print("Running Validation...")
@@ -260,39 +264,76 @@ def train(model, optimizer, scheduler, epochs, train_dataloader, valid_dataloade
                 y_predict = model(input_ids, input_mask)
                 loss = torch.nn.functional.mse_loss(y_predict, label_ids)
             total_loss += loss.item()
-            avg_valid_loss = total_loss / len(valid_dataloader)
-            valid_loss_list.append(avg_valid_loss)
-            # Track the number of batches
-            nb_eval_steps += 1
-        print("  Average validation loss: {0:.2f}".format(total_loss / nb_eval_steps))
-        print("  Validation epoch took: {:}".format(format_time(time.time() - t0)))
+        avg_valid_loss = total_loss / len(valid_dataloader)
+        valid_loss_list.append(avg_valid_loss)
+        print(" Average validation loss: {0:.5f}".format(avg_valid_loss))
+        print(" Validation epoch took: {:}".format(format_time(time.time() - t0)))
 
         if best_valid_loss > avg_valid_loss:
             best_valid_loss = avg_valid_loss
-            save_checkpoint(save_file_path + '/model.pt', model, best_valid_loss)
-            save_metrics(save_file_path + '/metrics.pt', train_loss_list, valid_loss_list,  range(epochs))
-    save_metrics(save_file_path + '/metrics.pt', train_loss_list, valid_loss_list, range(epochs))
+            save_checkpoint(save_file_path + '/models/model.pt', model, best_valid_loss)
+            save_metrics(save_file_path + '/models/metrics.pt', train_loss_list, valid_loss_list,  range(epochs))
+    save_metrics(save_file_path + '/models/metrics.pt', train_loss_list, valid_loss_list, range(epochs))
+    training_history_plot(save_file_path, train_loss_list, valid_loss_list, range(epochs))
     print("")
     print("Training complete!")
 
 
-def evaluate(model, test_dataloader, device):
-    # Prediction on test set
+def evaluate(model, test_dataloader, device, save_file_path='.'):
     print('Predicting returns on that day based on news begin on July 1, 2020.')
     # Put model in evaluation mode
     model.eval()
     # Tracking variables
-    pred_y, true_y = [], []
+    total_loss = 0
+    pred_y, true_y = np.array([]), np.array([])
     # Predict
     for _, test_batch in enumerate(test_dataloader):
         batch = tuple(inp.to(device) for inp in test_batch)
         input_ids, input_mask, label_ids = batch
         with torch.no_grad():
-            y_pred = model(input_ids, attention_mask=input_mask)
-        y_pred = y_pred.detach().cpu().numpy()
-        label_ids = label_ids.to('cpu').numpy()
-        pred_y.extend(y_pred)
-        true_y.extend(label_ids)
+            y_pred = model(input_ids, input_mask)
+            loss = torch.nn.functional.mse_loss(y_pred, label_ids)
+        total_loss += loss.item()
+        y_pred = y_pred.cpu().numpy()*10
+        label_ids = label_ids.cpu().numpy()*10
+        pred_y = np.append(pred_y, y_pred)
+        true_y = np.append(true_y, label_ids)
 
-
+    print("  Average test loss: {0:.2f}".format(total_loss / len(test_dataloader)))
     print('DONE.')
+    pro = ts.pro_api('0d52800e5aed61cb4188bfde75dceff83fef0a928a0363a12a3c27d2')
+    szzz = pro.index_daily(ts_code='000001.SH', start_date='20200701', end_date='20210101')
+    szzz = szzz[['trade_date', 'close', 'pct_chg']].astype({'trade_date': 'datetime64', 'close':'float', 'pct_chg': 'float'})
+    szzz = szzz.sort_values(by=['trade_date'], ascending=True)
+    szzz['predict'] = pred_y
+    szzz['true_pct_chg'] = true_y
+    szzz['diff'] = (szzz['predict'] - szzz['true_pct_chg']).abs()
+    szzz.to_excel(save_file_path+'/evaluation/predict.xlsx', index=False)
+    max_date = szzz[['trade_date','predict','true_pct_chg','diff']].loc[np.abs(szzz['diff']).idxmax(),:]
+    diff_info = szzz['diff'].describe()
+
+    my_dpi = 120
+    fig, ax1 = plt.subplots(figsize=(1000 / my_dpi, 600 / my_dpi), dpi=my_dpi)
+    ax2 = ax1.twinx()
+    ax1.plot(szzz['trade_date'], szzz['predict'], label='predict', color='red')
+    ax1.plot(szzz['trade_date'], szzz['true_pct_chg'], label='true', color='blue')
+    ax1.vlines(max_date['trade_date'], ymin=szzz['diff'].min(), ymax=szzz['diff'].max(), color='green')
+    ax1.text(x=max_date['trade_date'], y=szzz['diff'].min()*0.8, s='residual_max'+str(round(max_date['diff'],2)), color='gray')
+
+    ax1.hlines(diff_info['mean'], xmin=szzz['trade_date'].min(), xmax=szzz['trade_date'].max(), color='green')
+    ax1.text(x=szzz['trade_date'].max(), y=diff_info['mean'], s='residual_mean'+str(round(diff_info['mean'],2)), color='green')
+    ax1.hlines(diff_info['25%'], xmin=szzz['trade_date'].min(), xmax=szzz['trade_date'].max(), color='green')
+    ax1.text(x=szzz['trade_date'].max(), y=diff_info['25%'], s='residual_lower_quartile'+str(round(diff_info['25%'],2)), color='green')
+    ax1.hlines(diff_info['75%'], xmin=szzz['trade_date'].min(), xmax=szzz['trade_date'].max(), color='green')
+    ax1.text(x=szzz['trade_date'].max(), y=diff_info['75%'], s='residual_upper_quartile'+str(round(diff_info['75%'],2)), color='green')
+
+    ax2.plot(szzz['trade_date'], szzz['close'], label='szzz', color='black')
+    plt.title('The predict and true return of szzz from July 1, 2020')
+    ax1.set_xlabel('')
+    ax1.set_ylabel('Return(%)')
+    ax2.set_ylabel('SH.000001')
+    lines, labels = ax1.get_legend_handles_labels()
+    lines2, labels2 = ax2.get_legend_handles_labels()
+    ax2.legend(lines + lines2, labels + labels2, loc=0)
+    plt.savefig(save_file_path+'/evaluation/evalution.png')
+    plt.show()
